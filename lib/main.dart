2235ead99e04f27817ea7e1d4b9d563ffcbce24a
Heitor <heitordiastavares3@gmail.com>
2024-06-04 19:57:13 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:project_av3/login.page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Project AV3',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+    );
+  }
+}
